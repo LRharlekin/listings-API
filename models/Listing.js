538e7fa211c038f0fbde2dc6bbd8a4e8baf032a8
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2023-12-30 02:07:44 +0200

    Listing schema reference to User schema, mongoose.Types.ObjectId

---
+++ models/Listing.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const ListingSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      require: [true, "Please provide company name."],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position."],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declinded", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user."],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Listing", ListingSchema);
