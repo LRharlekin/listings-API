efd23a0b27ed54f0c2c862d3b4ff5ae9aae27b23
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2023-12-31 22:59:26 +0200

    updateListing controller

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,6 +1,7 @@
 const Listing = require("../models/Listing");
 const asyncWrapper = require("../middlewares/async-wrapper");
 const { StatusCodes } = require("http-status-codes");
+const { NotFoundError } = require("../errors");
 
 const getAllListings = async (req, res) => {
   res.send("get all listings");
@@ -12,9 +13,18 @@ const createListing = asyncWrapper(async (req, res) => {
   const listingToAdd = await Listing.create(req.body);
   res.status(StatusCodes.CREATED).json({ listingToAdd });
 });
-const updateListing = async (req, res) => {
-  res.send("update or edit a listing");
-};
+const updateListing = asyncWrapper(async (req, res) => {
+  const { id: listingID } = req.params;
+  const listing = await Listing.findByIdAndUpdate(listingID, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!listing) {
+    throw new NotFoundError(`No listing with id: ${listingID} found.`);
+  }
+  res.status(StatusCodes.OK).json({ listing });
+});
+
 const deleteListing = async (req, res) => {
   res.send("delete listing");
 };
