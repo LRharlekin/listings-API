696d23d87c8700653e03c09f542b204665d47aaa
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2023-12-31 22:41:52 +0200

    async wrapper middleware

---
+++ middlewares/async-wrapper.js
@@ -0,0 +1,10 @@
+const asyncWrapper = (controllerFn) => {
+  return async (req, res, next) => {
+    try {
+      await controllerFn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+module.exports = asyncWrapper;
