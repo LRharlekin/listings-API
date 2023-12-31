de8b5446dce3d54372210c89cd57476eddd032a5
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2023-12-31 22:45:56 +0200

    createListing controller added

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,12 +1,17 @@
+const Listing = require("../models/Listing");
+const asyncWrapper = require("../middlewares/async-wrapper");
+const { StatusCodes } = require("http-status-codes");
+
 const getAllListings = async (req, res) => {
   res.send("get all listings");
 };
 const getListing = async (req, res) => {
   res.send("get listing");
 };
-const createListing = async (req, res) => {
-  res.json(req.user);
-};
+const createListing = asyncWrapper(async (req, res) => {
+  const listingToAdd = await Listing.create(req.body);
+  res.status(StatusCodes.CREATED).json({ listingToAdd });
+});
 const updateListing = async (req, res) => {
   res.send("update or edit a listing");
 };
