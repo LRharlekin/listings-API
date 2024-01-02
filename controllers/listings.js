3c2fb4e39c5bd14beebfa3b7e49ce5316f46e56b
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2024-01-02 12:09:07 +0200

    refactor order of controller functions to represent CRUD

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,6 +3,14 @@ const asyncWrapper = require("../middlewares/async-wrapper");
 const { StatusCodes } = require("http-status-codes");
 const { NotFoundError, BadRequestError } = require("../errors");
 
+// CRUD operation - CREATE
+const createListing = async (req, res) => {
+  req.body.createdBy = req.user.userID;
+  const listingToAdd = await Listing.create(req.body);
+  res.status(StatusCodes.CREATED).json({ listingToAdd });
+};
+
+// CRUD operation - READ
 const getAllListings = async (req, res) => {
   // We're actually not looking for "A.L.L." the jobs, but only those associated with the authorized user
   const listings = await Listing.find({ createdBy: req.user.userID }).sort(
@@ -10,9 +18,10 @@ const getAllListings = async (req, res) => {
   );
   res.status(StatusCodes.OK).json({ listings, count: listings.length });
 };
+
+// CRUD operation - READ
 const getListing = async (req, res) => {
   // nested destructuring syntax!
-
   const {
     user: { userID },
     // give the destructured listing ID an appropriate alias
@@ -27,32 +36,30 @@ const getListing = async (req, res) => {
   }
   res.status(StatusCodes.OK).json({ listing });
 };
-const createListing = async (req, res) => {
-  req.body.createdBy = req.user.userID;
-  const listingToAdd = await Listing.create(req.body);
-  res.status(StatusCodes.CREATED).json({ listingToAdd });
+
+// CRUD operation - UPDATE
+const updateListing = async (req, res) => {
+  res.send("update listing");
+  // const { id: listingID } = req.params;
+  // const listing = await Listing.findByIdAndUpdate(listingID, req.body, {
+  //   new: true,
+  //   runValidators: true,
+  // });
+  // if (!listing) {
+  //   throw new NotFoundError(`No listing with id: ${listingID} found.`);
+  // }
+  // res.status(StatusCodes.OK).json({ listing });
 };
 
-const updateListing = asyncWrapper(async (req, res) => {
-  const { id: listingID } = req.params;
-  const listing = await Listing.findByIdAndUpdate(listingID, req.body, {
-    new: true,
-    runValidators: true,
-  });
-  if (!listing) {
-    throw new NotFoundError(`No listing with id: ${listingID} found.`);
-  }
-  res.status(StatusCodes.OK).json({ listing });
-});
-
+// CRUD operation - DELETE
 const deleteListing = async (req, res) => {
   res.send("delete listing");
 };
 
 module.exports = {
+  createListing,
   getAllListings,
   getListing,
-  createListing,
   updateListing,
   deleteListing,
 };
